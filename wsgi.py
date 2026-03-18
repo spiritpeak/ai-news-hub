4abcfbbb37cb0f6940cbe7703a5feb6143c38c58
spiritpeakok <747281589@qq.com>
2026-03-18 16:23:20 +0800

    使用根目录 wsgi.py 替代 your_application 包

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,10 @@
-from main import app as application
+# -*- coding: utf-8 -*-
+"""
+WSGI 入口文件 - 简单版本
+直接导入 main 模块，避免任何复杂的导入逻辑
+"""
+
+# 直接导入 app 对象
+import main
+application = main.app
+app = application
