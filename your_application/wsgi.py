b62f3dec91728c6589353aae15eb57449ccf9986
spiritpeakok <747281589@qq.com>
2026-03-18 15:08:48 +0800

    添加 your_application/wsgi.py 入口，适配 Render 默认启动方式

---
+++ your_application/wsgi.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+WSGI 入口文件
+Render 默认会导入 your_application.wsgi 并查找 application 对象
+"""
+
+# 从 backend.main 导入 FastAPI 应用
+from backend.main import app as application
+
+# 同时导出 app 以便兼容
+app = application
