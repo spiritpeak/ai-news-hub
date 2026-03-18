575d61d1e668520bd2f9cf4232ee9b0b0d1e14ed
spiritpeakok <747281589@qq.com>
2026-03-18 16:01:11 +0800

    修复导入：直接从根目录导入 main 模块

--- your_application/wsgi.py
+++ your_application/wsgi.py
@@ -2,16 +2,10 @@
 """
 WSGI 入口文件
 Render 默认会导入 your_application.wsgi 并查找 application 对象
+代码文件在主目录下，直接导入 main 即可
 """
 
-import sys
-import os
-
-# 添加项目根目录到 Python 路径
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# 从 backend.main 导入 FastAPI 应用
-from backend.main import app as application
+from main import app as application
 
 # 同时导出 app 以便兼容
 app = application
