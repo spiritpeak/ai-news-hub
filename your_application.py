29011de484eec904fbecc62a03c692b5cd466bcb
spiritpeakok <747281589@qq.com>
2026-03-18 17:17:27 +0800

    创建 your_application.py 单文件入口

--- your_application.py
+++ your_application.py
@@ -1,1 +1,15 @@
-from main import app as application
+# -*- coding: utf-8 -*-
+"""
+your_application.py - Render 默认入口
+Render 默认执行 gunicorn your_application.wsgi
+这个文件提供 wsgi 应用
+"""
+
+# 延迟导入 main 模块
+def get_app():
+    import main
+    return main.app
+
+# 导出 application 对象
+application = get_app()
+app = application
